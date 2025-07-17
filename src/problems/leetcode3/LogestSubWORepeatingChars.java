fb78d270e7cf9346fa16492a64a390b7c0c9291e
dalton <daltonmotene@gmail.com>
2025-07-17 23:11:01 +0200

    Attempt

---
+++ src/problems/leetcode3/LogestSubWORepeatingChars.java
@@ -0,0 +1,22 @@
+package problems.leetcode3;
+
+import java.util.HashMap;
+
+public class LogestSubWORepeatingChars {
+    public int solution(String s){
+        
+        int result = 0, left = 0, n = s.length();
+
+        HashMap<Character, Integer> map = new HashMap<>();
+        
+        for (int j = 0; j < n; j++){
+            if(map.containsKey(s.charAt(j))){
+                left = Math.max(map.get(s.charAt(j)), left);
+            }
+            result = Math.max(result, j - left + 1);
+            map.put(s.charAt(j), j + 1);
+        }
+        
+        return  result;
+    }
+}
